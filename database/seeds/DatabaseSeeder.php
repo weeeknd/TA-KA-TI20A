16f56f66af8fb8ead98c90bf93543f81673708a8
weeeknd <reza.aditya0092@gmail.com>
2024-01-06 18:01:07 +0700

    Mengupdate Folder Database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,40 +5,14 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // SUSUNAN DIBAWAH INI JANGAN DIUBAH !! NANTI ERROR
-
-        $this->call(UsersTableSeeder::class);
-
-        // Seeder Bagian Data Civitas
-        $this->call(AgamaTableSeeder::class);
-        $this->call(WilayahTableSeeder::class);
-        $this->call(CivitasTableSeeder::class);
-        $this->call(SiswaTableSeeder::class);
-        $this->call(PegawaiTableSeeder::class);
-        $this->call(TendikTableSeeder::class);
-        $this->call(GuruTableSeeder::class);
-        $this->call(PesanTableSeeder::class);
-
-        // Seeder Bagian Pekerjaan Guru
-        $this->call(TipeTableSeeder::class);
-        $this->call(MataPelajaranTableSeeder::class);
-        $this->call(TahunAjaranTableSeeder::class);
-        $this->call(KelasTableSeeder::class);
-        $this->call(KBMTableSeeder::class);
-        // $this->call(PekerjaanGuruTableSeeder::class);
-
-        // Seeder Bagian Inventaris
-        $this->call(JenisRuanganTableSeeder::class);
-        $this->call(JenisInventarisTableSeeder::class);
-        $this->call(GedungTableSeeder::class);
-        $this->call(RuanganTableSeeder::class);
-        $this->call(KebutuhanBarangTableSeeder::class);
-        $this->call(InventarisTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(SiswaSeeder::class);
     }
 }
